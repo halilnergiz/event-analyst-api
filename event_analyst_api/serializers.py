26a16bbd6938c94383c94f7320f5a5d73fcd8712
halilnergiz <halil123911@gmail.com>
2024-05-07 22:27:25 +0300

    user email verification structure implemented

--- event_analyst_api/serializers.py
+++ event_analyst_api/serializers.py
@@ -8,13 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
-    def create(self, validated_data):
-        user = CustomUser(
-            username=validated_data["username"], email=validated_data["email"]
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
+        def create(self, validated_data):
+            user = CustomUser(
+                username=validated_data["username"], email=validated_data["email"]
+            )
+            user.set_password(validated_data["password"])
+            user.save()
+            return user
 
 
 class ChangePasswordSerializer(serializers.Serializer):
@@ -24,3 +24,11 @@ class ChangePasswordSerializer(serializers.Serializer):
 
 class ResetPasswordEmailSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = CustomUser
+        fields = ["token"]
--- event_analyst_api/urls.py
+++ event_analyst_api/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import register_user, user_login, user_logout, change_password
+from .views import (
+    register_user,
+    user_login,
+    user_logout,
+    change_password,
+    VerifyEmail,
+    ResendActivationEmail,
+)
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('change_password/', change_password, name='change_password'),
+    path("register/", register_user, name="register"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("change_password/", change_password, name="change_password"),
+    path("email_verify/", VerifyEmail.as_view(), name="email_verify"),
+    path(
+        "resend_email_verify/",
+        ResendActivationEmail.as_view(),
+        name="resend_email_verify",
+    ),
 ]
