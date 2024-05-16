2eadb419fa01fe80d5c01a523c6b105da774bc2b
halilnergiz <halil123911@gmail.com>
2024-05-16 04:51:14 +0300

    fix: show photo via url bug fixed

--- event_analyst/urls.py
+++ event_analyst/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
         "api/password_reset/",
         include("django_rest_passwordreset.urls", namespace="password_reset"),
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
