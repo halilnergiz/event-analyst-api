4858c8b8cae121f3cd33775c15f1a422702dc906
halilnergiz <halil123911@gmail.com>
2024-05-29 15:41:23 +0300

    fix: reset email routing fixed

--- event_analyst_api/signals.py
+++ event_analyst_api/signals.py
@@ -13,9 +13,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         'current_user': reset_password_token.user,
         'username': reset_password_token.user.username,
         'email': reset_password_token.user.email,
-        'reset_password_url': "{}?token={}".format(
-            instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
-            reset_password_token.key)
+        'reset_password_url': "http://localhost:3000/password-reset/?token={}".format(reset_password_token.key)
     }
 
     # render email text
